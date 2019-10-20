0a90a12af5e8299c637c9d82e1b522eecf49aae3
kelly r maass <kmaass77627@gmail.com>
2019-10-20 20:28:31 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,8 +3,8 @@ def my_all?(collection)
   block_return_values = []
   while i < 
   collection.length 
-    block_return_values 
-    << yield(collection[i])
+    block_return_values<< 
+   yield(collection[i])
   i = i + 1 
 end
 block_return_values
